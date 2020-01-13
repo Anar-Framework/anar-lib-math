e6bc6e7fdb7b1d74076715f3291b61a868c7b4be
mohbadar <mohammadbadarhashimi@gmail.com>
2020-01-13 13:15:53 +0430

    math utility library

---
+++ src/main/java/af/gov/anar/lib/math/AnarLibMathApplication.java
@@ -0,0 +1,13 @@
+package af.gov.anar.lib.math;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnarLibMathApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnarLibMathApplication.class, args);
+	}
+
+}
---
+++ src/test/java/af/gov/anar/lib/math/AnarLibMathApplicationTests.java
@@ -0,0 +1,13 @@
+package af.gov.anar.lib.math;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnarLibMathApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
